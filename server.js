b037300af86e856064f8cb965e137066298c7a2c
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2020-04-15 14:55:55 +0700

    changing to db atlas
    still need some fix on uri

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // create express app
 const app = express();
 
 // parse requests of content-type
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+// this is for local db
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
+// this for mongoDB atlas and actually this is not save and
+// we need to create .env file so we can store the string
+const uri =
+  "mongodb+srv://admin:dbUserPassword@cluster0-pkarp.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
+// just change the 'dbConfig' to 'uri' if you want run locally
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
 
 // Require users routes
 // require('./app/routes/something.routes.js')(app);
-require('./app/routes/user.routes.js')(app);
+require("./app/routes/user.routes.js")(app);
 
 // define a simple route
-app.get('/', (req, res) => {
-    res.json({"message": "connected."});
+app.get("/", (req, res) => {
+  res.json({ message: "connected." });
 });
 
 // listen for requests
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
